beaad4e3b9b07a3b10112fd29bacce4fe2e54d9a
Toberfra <toberfra@online.de>
2020-01-17 17:18:27 +0100

    falsche Daten
    Signed-off-by: Toberfra <toberfra@online.de>

--- Software/Datenbank/dump-Getraenke.sql
+++ Software/Datenbank/dump-Getraenke.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `Ausgabe_Groesse`
 --
-
+use getraenke
 DROP TABLE IF EXISTS `Ausgabe_Groesse`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
